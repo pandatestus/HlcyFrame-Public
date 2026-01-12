cac6d9ccdfd0917c2df6b59469c42471a38d52bd
pandatestus <hendrikrinas14@gmail.com>
2026-01-12 17:20:09 +0100

    first

---
+++ src/main/java/de/panda/hlcyFramePublic/HlcyFramePublic.java
@@ -0,0 +1,18 @@
+package de.panda.hlcyFramePublic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HlcyFramePublic extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        //test
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
